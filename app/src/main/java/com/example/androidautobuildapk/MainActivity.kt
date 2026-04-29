3cecf8dbf9c3043be8ddd2618703d379bd1f628e
anthony3d <anthony3d@yahoo.com>
2026-04-29 20:32:11 +0300

    Update MainActivity.kt

--- app/src/main/java/com/example/androidautobuildapk/MainActivity.kt
+++ app/src/main/java/com/example/androidautobuildapk/MainActivity.kt
@@ -1,11 +1,16 @@
 package com.example.repview
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(TextView(this).apply {
+            text = "Нажмите 'Поделиться' в вашем приложении с отчетами\nи выберите RepView"
+            textSize = 18f
+            setPadding(50, 50, 50, 50)
+        })
     }
 }
